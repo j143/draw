7347c42d1eae0bd6b7e530daaef64857edd60441
Herz, Andreas <andreas.herz@sap.com>
2016-03-21 13:41:41 +0100

    add missing _this reference

--- src/assets/javascript/io/BackendStorage.js
+++ src/assets/javascript/io/BackendStorage.js
@@ -93,22 +93,7 @@ shape_designer.storage.BackendStorage = draw2d.storage.FileStorage.extend({
 
         $('#githubFileSelectDialog').modal('show');
     },
-    
-    load: function(fileId, successCallback, errorCallback){
-        var _this = this;
-        $.jsonRPC.request('findById', {
-            params: [fileId],
-            endPoint: _this.baseUrl+'rpc/Figure.php',
-            success: function(response) {
-                successCallback(response.result);
-            },
-            error: function(result) {
-                errorCallback();
-            }
-        });
-  
-    },
-    
+
     save: function(view, currentFileHandle, successCallback, errorCallback, abortCallback){
         var _this = this;
         
@@ -144,7 +129,7 @@ shape_designer.storage.BackendStorage = draw2d.storage.FileStorage.extend({
                         sha: currentFileHandle.sha
                     };
 
-                    currentRepository.contents(currentLoadedFile.path).add(config)
+                    _this.currentRepository.contents(currentLoadedFile.path).add(config)
                         .then(function(info) {
                             currentFileHandle.sha =  info.content.sha;
                             $('#githubCommitDialog').modal('hide');
