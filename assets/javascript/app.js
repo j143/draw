53999ea99b29ada235c6fe31e0ff1a0389f188f8
Andreas Herz <a.herz@freegroup.de>
2016-04-28 10:06:59 +0200

    Updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1104,6 +1104,9 @@ shape_designer.dialog.FigureTest = Class.extend(
     },
 
 	show:function(){
+		var _this = this;
+		this.animationFrameFunc = $.proxy(this._calculate,this);
+
 		var writer = new shape_designer.FigureWriter();
 		
 		writer.marshal(app.view, "testShape",function(js){
@@ -1128,6 +1131,7 @@ shape_designer.dialog.FigureTest = Class.extend(
 	        $("body").append(splash);
 
 			var canvas    = new draw2d.Canvas("test_canvas");
+			_this.canvas = canvas;
 			canvas.installEditPolicy( new draw2d.policy.canvas.ShowDotEditPolicy(20,1,"#FF4981"));
 			var router = new draw2d.layout.connection.InteractiveManhattanConnectionRouter();
 			canvas.installEditPolicy( new draw2d.policy.connection.ComposedConnectionCreatePolicy(
@@ -1166,27 +1170,60 @@ shape_designer.dialog.FigureTest = Class.extend(
 			// create and add two nodes which contains Ports (In and OUT)
 			//
 			var start = new draw2d.shape.node.Start();
-			var toggle = new shape_designer.figure.TestSwitch();
+			var toggle1 = new shape_designer.figure.TestSwitch();
+			var toggle2 = new shape_designer.figure.TestSwitch();
 			var end   = new draw2d.shape.node.End();
 
 			// ...add it to the canvas
-			canvas.add( toggle, 50,150);
+			canvas.add( toggle1, 50,150);
+			canvas.add( toggle2, 50,200);
 			canvas.add( start, 50,250);
 			canvas.add( end, 630,250);
 
 			canvas.setCurrentSelection(test);
 			var removeDialog = function(){
+				_this.simulate=false;
 				splash.removeClass("open");
 				setTimeout(function(){splash.remove();},400);
 			};
 
 			$("#test_close").on("click",removeDialog);
 			splash.addClass("open");
-		 });
 
-	}
+			_this.simulate = true;
+			requestAnimationFrame(_this.animationFrameFunc);
+		});
 
-      
+	},
+
+
+	_calculate:function()
+	{
+		// call the "calculate" method if given to calculate the output-port values
+		//
+		var figures = this.canvas.getFigures().clone().grep(function(f){
+			return f.calculate;
+		});
+		figures.each(function(i,figure){
+			figure.calculate();
+		});
+
+		// transport the value from oututPort to inputPort
+		//
+		this.canvas.getLines().each(function(i,line){
+			var outPort = line.getSource();
+			var inPort  = line.getTarget();
+			inPort.setValue(outPort.getValue());
+			line.setColor(outPort.getValue()?"#ff5252":"#0000ff");
+		});
+
+		if(this.simulate===true){
+			requestAnimationFrame(this.animationFrameFunc);
+		}
+	},
+
+
+
 });  
 shape_designer.dialog.FigureMarkdownEdit = Class.extend(
 {
