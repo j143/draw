df4adac5c5af213ad2c2f6076f693b3e390bd06f
Herz, Andreas <a.herz@freegroup.de>
2016-09-21 15:02:07 +0200

    enable HTML tags in the shape documentation

--- src/assets/javascript/dialog/FigureMarkdownEdit.js
+++ src/assets/javascript/dialog/FigureMarkdownEdit.js
@@ -8,39 +8,18 @@ shape_designer.dialog.FigureMarkdownEdit = Class.extend(
 
         this.lineHeight=1.45;
         this.defaults = {
-            html:         false,        // Enable HTML tags in source
+            html:         true,         // Enable HTML tags in source
             xhtmlOut:     false,        // Use '/' to close single tags (<br />)
             breaks:       false,        // Convert '\n' in paragraphs into <br>
             langPrefix:   'language-',  // CSS language prefix for fenced blocks
             linkify:      true,         // autoconvert URL-like texts to links
-            linkTarget:   '',           // set target to open link in
+            linkTarget:   '_blank',     // set target to open link in
             typographer:  true          // Enable smartypants and other sweet transforms
         };
 
     },
 
     /**
-
-
-# AND Gate
-
-## Description
-The **AND** gate is a basic digital logic gate that implements logical conjunction - it behaves according to the truth table on the bottom.
-
-A HIGH output results only if both the inputs to the AND gate are HIGH. If neither or only one input to the AND gate is HIGH, a LOW output results. In another sense, the function of AND effectively finds the minimum between two binary digits, just as the OR function finds the maximum.
-
-Therefore, the output is always 0 except when all the inputs are 1.
-
-## Logic table
-
-| INPUT 1   | INPUT   |  OUTPUT    |
-|:---------:|:-------:|:----------:|
-| Low       | Low     |  Low       |
-| High      | Low     |  Low       |
-| Low       | High    |  Low       |
-| High      | High    |  High      |
-
-
      */
 	show:function()
     {
